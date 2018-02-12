356bdfc907d694f1fd9c74ea7c9aff34fb5dcd86
VaughnA <vaughn_arjavac@davincischools.org>
2018-02-12 13:13:20 -0800

    Update Frolic.rb

--- Frolic.rb
+++ Frolic.rb
@@ -1,10 +1,10 @@
 # Name Of Song: Frolic (Curb Your Enthusiasm Theme)
 # Sheet Music: https://store.gogustaf.com/en/article/msg-cd65ce2e9cdb0d8382d00280ec269fb1/Frolic-theme-from-Curb-Your-Enthusiasm-Luciano-Michelini-Piano-Movie-Tv-Games
 # BPM Used: https://store.gogustaf.com/en/article/msg-cd65ce2e9cdb0d8382d00280ec269fb1/Frolic-theme-from-Curb-Your-Enthusiasm-Luciano-Michelini-Piano-Movie-Tv-Games
-use_bpm 75
+use_bpm 60
 use_synth :piano
 
-live_loop :Frolic do
+ive_loop :Frolic do
   play :G2
   sleep 0.6
   play :A2
@@ -55,4 +55,53 @@ live_loop :Frolic do
   sleep 0.4
   play :as4
   sleep 0.4
+  play :b4
+  sleep 0.4
+  play :g5
+  sleep 0.8
+  play :b2
+  sleep 0.8
+  play :a4
+  play :bb2
+  sleep 0.4
+  play :a4
+  sleep 0.3
+  play :ab4
+  sleep 0.2
+  play :g4
+  play :f2
+  sleep 0.3
+  play :g4
+  sleep 0.3
+  play :fs4
+  sleep 0.4
+  play :a4
+  play :bb2
+  sleep 0.3
+  play :fs4
+  sleep 0.2
+  play :g4
+  sleep 0.4
+  play :g4
+  play :f2
+  sleep 0.3
+  play :fs4
+  sleep 0.2
+  play :g4
+  sleep 0.3
+  play :gs4
+  sleep 0.3
+  play :a4
+  play :a2
+  sleep 0.3
+  play :a4
+  sleep 0.3
+  play :gs4
+  sleep 0.2
+  play :g4
+  play :e2
+  sleep 0.3
+  play :g4
+  sleep 0.3
+  play :gs4
 end
