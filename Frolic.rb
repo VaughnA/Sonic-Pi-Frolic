b299f1a9bfdcfb826fca60d2df01fd4d5da25d65
VaughnA <vaughn_arjavac@davincischools.org>
2018-02-14 14:43:07 -0800

    Update Frolic.rb

--- Frolic.rb
+++ Frolic.rb
@@ -1,107 +1,111 @@
 # Name Of Song: Frolic (Curb Your Enthusiasm Theme)
 # Sheet Music: https://store.gogustaf.com/en/article/msg-cd65ce2e9cdb0d8382d00280ec269fb1/Frolic-theme-from-Curb-Your-Enthusiasm-Luciano-Michelini-Piano-Movie-Tv-Games
 # BPM Used: https://store.gogustaf.com/en/article/msg-cd65ce2e9cdb0d8382d00280ec269fb1/Frolic-theme-from-Curb-Your-Enthusiasm-Luciano-Michelini-Piano-Movie-Tv-Games
-use_bpm 60
+se_bpm 120
 use_synth :piano
 
-ive_loop :Frolic do
+
+live_loop :Frolic do
   play :G2
-  sleep 0.6
+  sleep 1
   play :A2
-  sleep 0.6
+  sleep 1
   play :B2
-  sleep 0.6
+  sleep 1
   play :C3
   play :B4
-  sleep 0.4
+  sleep 1
   play :B4
-  sleep 0.4
+  sleep 0.75
   play :Bb4
-  sleep 0.2
+  sleep 0.25
   play :a4
   play :g2
-  sleep 0.4
+  sleep 1
   play :a4
-  sleep 0.4
+  sleep 0.75
   play :gs4
-  sleep 0.4
+  sleep 0.25
   play :b4
   play :g2
-  sleep 0.4
+  sleep 0.75
   play :gs4
-  sleep 0.2
+  sleep 0.25
   play :a4
-  sleep 0.4
+  sleep 1
   play :a4
   play :g2
-  sleep 0.4
+  sleep 0.75
   play :gs4
-  sleep 0.2
+  sleep 0.25
   play :a4
-  sleep 0.4
+  sleep 0.75
   play :as4
-  sleep 0.4
+  sleep 0.25
   play :b4
   play :b2
-  sleep 0.4
+  sleep 1
   play :b4
-  sleep 0.4
+  sleep 0.75
   play :as4
-  sleep 0.2
+  sleep 0.25
   play :a4
   play :fs2
-  sleep 0.4
+  sleep 1
   play :a4
-  sleep 0.4
+  sleep 0.75
   play :as4
-  sleep 0.4
+  sleep 0.25
   play :b4
-  sleep 0.4
+  sleep 1
   play :g5
-  sleep 0.8
+  sleep 1
   play :b2
-  sleep 0.8
+  sleep 1
   play :a4
   play :bb2
-  sleep 0.4
+  sleep 1
   play :a4
-  sleep 0.3
+  sleep 0.75
   play :ab4
-  sleep 0.2
+  sleep 0.25
   play :g4
   play :f2
-  sleep 0.3
+  sleep 1
   play :g4
-  sleep 0.3
+  sleep 0.75
   play :fs4
-  sleep 0.4
+  sleep 0.25
   play :a4
   play :bb2
-  sleep 0.3
+  sleep 0.75
   play :fs4
-  sleep 0.2
+  sleep 0.25
   play :g4
-  sleep 0.4
+  sleep 1
   play :g4
   play :f2
-  sleep 0.3
+  sleep 0.75
   play :fs4
-  sleep 0.2
+  sleep 0.25
   play :g4
-  sleep 0.3
+  sleep 0.75
   play :gs4
-  sleep 0.3
+  sleep 0.25
   play :a4
   play :a2
-  sleep 0.3
+  sleep 1
   play :a4
-  sleep 0.3
+  sleep 0.75
   play :gs4
-  sleep 0.2
+  sleep 0.25
   play :g4
   play :e2
-  sleep 0.3
+  sleep 1
   play :g4
-  sleep 0.3
+  sleep 0.75
   play :gs4
+  sleep 0.25
 end
+
+ 
